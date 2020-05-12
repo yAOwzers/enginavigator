ac618eeacb73e68c24bafa5a3e1a89cec09ac7af
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-05-12 15:41:53 +0800

    types of functions

--- learning/neilbaner/js/js-basics/index.js
+++ learning/neilbaner/js/js-basics/index.js
@@ -2,5 +2,15 @@ function greet(name, lastName) {
     console.log("Hello " + name + " " + lastName);
 }
 
+// calculates a value
+function square(number){
+    return number * number;
+}
+
+let number = square(2);
+
+console.log(number)
+
+console.log(square(2))
 greet("Neil","Banerjee")
 greet("Pallav","Agarwal")
