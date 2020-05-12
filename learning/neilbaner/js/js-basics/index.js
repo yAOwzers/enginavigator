746239c868dad3325aa18a5acdb407fb988a8465
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-05-12 15:06:33 +0800

    variables

--- learning/neilbaner/js/js-basics/index.js
+++ learning/neilbaner/js/js-basics/index.js
@@ -1,2 +1,13 @@
 // First JS code
 console.log("Hello World");
+
+// variables
+let name = "Neil";
+console.log(name);
+// names cannot be reserved keywords: let, if, else, var etc. 
+// names should be meaningful, not a, b, a1, x, etc. 
+// names cannot start with a number
+// names cannot contain spaces or hyphens
+// names are case sensitive
+let firstName = "Neil";
+let lastName = "Banerjee";
