50df354df859baf981899d9e101eb53c936cba43
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-05-13 11:53:47 +0800

    file system module

--- learning/neilbaner/node/first-app/app.js
+++ learning/neilbaner/node/first-app/app.js
@@ -1,7 +1,10 @@
-const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// const files = fs.readdirSync('./');
 
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
+// console.log(files);
+
+fs.readdir('$', function(err, files) {
+    if(err) console.log('Error', err);
+    else console.log('Result', files);
+});
