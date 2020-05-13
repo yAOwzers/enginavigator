708361e172c5a7f86263b62e2a029b0f8db50792
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-05-13 15:37:42 +0800

    more events

--- learning/neilbaner/node/first-app/app.js
+++ learning/neilbaner/node/first-app/app.js
@@ -1,8 +1,11 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
+
 emitter.on('messageLogged', function(arg){
     console.log('Listener called', arg);
 });
 
-emitter.emit('messageLogged', {id: 1, url: 'http://'});
+const log = require('./logger');
+
+log('message');
--- learning/neilbaner/node/first-app/logger.js
+++ learning/neilbaner/node/first-app/logger.js
@@ -1,12 +1,20 @@
-console.log(__filename);
-console.log(__dirname);
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 var url = 'http://mylogger.io/log';
 
-function log(message){
-    // send an http request
-    console.log(message);
+class Logger {
+    log(message) {
+        // send an http request
+        console.log(message);
+    
+        emitter.emit('messageLogged', {
+            id: 1,
+            url: 'http://'
+        });
+    }
 }
+
 
 module.exports = log;
 // module.exports.endPoint = url; // implementation detail, no need for other modules to see this
