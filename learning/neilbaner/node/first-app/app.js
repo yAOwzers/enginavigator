7af96d60531cfd1d5a36eedae1481bdf3eaf2fea
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-05-13 12:08:45 +0800

    event arguments

--- learning/neilbaner/node/first-app/app.js
+++ learning/neilbaner/node/first-app/app.js
@@ -1,8 +1,8 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', function(arg){
+    console.log('Listener called', arg);
 });
 
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
