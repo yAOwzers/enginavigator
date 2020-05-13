e51ea259a231613205725ee8d5d77cc69e21fba3
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-05-13 10:50:56 +0800

    modules

--- learning/neilbaner/node/first-app/app.js
+++ learning/neilbaner/node/first-app/app.js
@@ -1,15 +1,1 @@
-// console.log(); // console is global object, available in any file
-
-// setTimeout();// call a function after a delay
-// clearTimeout();
-
-// setInterval(); // call a function repeatedly after a delay
-// clearInterval(); // stop calling the function
-
-// // window.console.log(); // no window object in node, but in browser, everything is a part of 
-
-// global.console.log(); // everything is under the global object in node
-
-var message = ''; // this is not global, only part of this file
-
-console.log(global.message);
+console.log(module); // module is not a global object
