1fb386204674f88bd19a26ab2050a2b7326b012a
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-05-22 14:34:05 +0800

    oh no oh no oh no it's a timing problem

--- learning/neilbaner/prototype-dijkstra/index.js
+++ learning/neilbaner/prototype-dijkstra/index.js
@@ -5,59 +5,104 @@ const dbDbName = 'graph_test';
 
 let sql = require('mysql');
 
-class Graph{
-    constructor(nodes, edges){
+class Graph {
+    constructor(nodes, edges) {
         this.nodes = nodes;
         this.edges = edges;
     }
-    shortestPath(start, end){
+    shortestPath(start, end) {
         return;
     }
-    printEdges(){
-        console.log("Edges:")
-        for(let edge of this.edges){
+    printEdges() {
+        console.log("Edges:");
+        for (let edge of this.edges) {
             console.log("Edge " + edge.id + " starts from " + edge.start + " and ends at " + edge.end + ". ");
         }
     }
-    printNodes(){
-        console.log("Nodes:")
-        for(let node of this.nodes){
+    printNodes() {
+        console.log("Nodes:");
+        for (let node of this.nodes) {
             console.log("Node " + node.id + ", " + node.name + ", on floor " + node.floor + " of block " + node.block + ". ");
         }
     }
 }
 
-class Edge{
-    constructor(id, start, end, weight, stairs){
+class Edge {
+    constructor(id, start, end, weight, stairs) {
         this.id = id;
         this.start = start;
         this.end = end;
         this.weight = weight;
         this.stairs = stairs;
     }
-    printEdge(){
+    printEdge() {
         console.log("Edge " + this.id + " starts from " + this.start + " and ends at " + this.end + ". ");
     }
 }
 
-class Node{
-    constructor(id, floor, block, name){
+class Node {
+    constructor(id, floor, block, name) {
         this.id = id;
         this.floor = floor;
         this.block = block;
         this.name = name;
     }
-    printNode(){
+    printNode() {
         console.log("Node " + this.id + ", " + this.name + ", on floor " + this.floor + " of block " + this.block + ". ");
     }
 }
 
-let edge1 = new Edge(1, 1, 2, 5, 0);
-let edge2 = new Edge(2, 2, 1, 3, 0);
-let node1 = new Node(1, '1', 'E1', 'E1-01-01');
-let node2 = new Node(2, '1', 'E1', 'E1-01-02');
+function oopTest() {
+    let edge1 = new Edge(1, 1, 2, 5, 0);
+    let edge2 = new Edge(2, 2, 1, 3, 0);
+    let node1 = new Node(1, '1', 'E1', 'E1-01-01');
+    let node2 = new Node(2, '1', 'E1', 'E1-01-02');
 
-let graph = new Graph([node1, node2], [edge1, edge2]);
+    let graph = new Graph([node1, node2], [edge1, edge2]);
 
-graph.printEdges();
-graph.printNodes();
+    graph.printEdges();
+    graph.printNodes();
+}
+
+let conn = sql.createConnection({
+    host: dbURI,
+    user: dbUsername,
+    password: dbPassword,
+    database: dbDbName
+});
+
+let graphNodes = [], graphEdges = [];
+
+conn.query('SELECT * FROM nodes;', (err, result, fields)=>{
+    if(err){
+        throw err;
+    }
+    console.log(result);
+    result.forEach((node)=>{
+        graphNodes.push(new Node(node.node_id, node.floor, node.block, node.name));
+    });
+});
+
+conn.query('SELECT * FROM edges;', (err, result, fields)=>{
+    if (err){
+        throw err;
+    }
+    console.log(result);
+    result.forEach((edge)=>{
+        graphEdges.push(new Edge(edge.edge_id, edge.start, edge.end, edge.weight, edge.stairs));
+    });
+})
+
+setTimeout(()=>{
+    console.log('hi');
+    graph = new Graph(graphNodes, graphEdges);
+    graph.printEdges();
+    graph.printNodes();
+}, 3000);
+
+
+conn.end((err)=>{
+    if(err){
+        throw err;
+    }
+});
