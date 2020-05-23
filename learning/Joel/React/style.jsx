a71b4670976c6037c2c7c2dd0d8ed1ee9bd864d4
J Neo <57039314+yAOwzers@users.noreply.github.com>
2020-05-24 05:41:51 +0800

    Use of className and style

---
+++ learning/Joel/React/style.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  }; //includes any data that this component needs
+
+  //define a property
+  styles = {
+    fontSize: 50,
+    fontWight: "bold",
+  };
+
+  //src = {} renders a dynamic image
+  //className -> bootstrap documentation
+  render() {
+    return (
+      <div>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount}
+        </span>
+        <button className="btn btn-secondary btn-sm">Incremenet</button>
+      </div>
+    );
+  }
