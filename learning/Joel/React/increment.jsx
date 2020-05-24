5eb613cf8bdb7372e77b45df4027fba1a1200aba
J Neo <57039314+yAOwzers@users.noreply.github.com>
2020-05-24 12:13:13 +0800

    handling events

---
+++ learning/Joel/React/increment.jsx
@@ -0,0 +1,17 @@
+  handleIncrement() {
+    console.log("Increment Clicked");
+  }
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Incremenet
+        </button>
+      </div>
+    );
+  }
