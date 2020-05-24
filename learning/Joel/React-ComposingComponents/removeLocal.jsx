6668bf1994aaaa81223a29a4d6a052bec10dae4f
J Neo <57039314+yAOwzers@users.noreply.github.com>
2020-05-24 19:04:05 +0800

    Remove Local State of components
    The child component is entirely controlled by their parent.
    Receives all the data via props
    Raises events whenever data needs to be changed

---
+++ learning/Joel/React-ComposingComponents/removeLocal.jsx
@@ -0,0 +1,94 @@
+In Counter:
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.props.counter; //object destructuring
+    return count === 0 ? "Zero" : count;
+  }
+------------------------------------------------------------------------------------------------------------------------
+In Counters:
+
+class Counters extends Component {
+  state = {
+    //does not remove a counter from the array,
+    //instead create a new array without a given counter.
+    //Call the set state method of the component and
+    //let React update the state
+
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id != counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    //value and selected will be the property of that props object
+    //our counter object contains all the relevant information of the counter
+    return (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
+}
