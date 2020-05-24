6d28537198f30b2a4c6c2c141d546f8eecec3fe8
J Neo <57039314+yAOwzers@users.noreply.github.com>
2020-05-24 12:06:17 +0800

    conditional rendering
    use of truthy and falsy in  {this.state.tags.length === 0 && "Please create a new tag!"}

---
+++ learning/Joel/React/condition.jsx
@@ -0,0 +1,20 @@
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
+  }
