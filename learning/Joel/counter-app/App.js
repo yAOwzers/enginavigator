57c45f78343ac0d1784f298dced92a5a5f3829b2
J Neo <57039314+yAOwzers@users.noreply.github.com>
2020-05-26 01:54:00 +0800

    Add files via upload

---
+++ learning/Joel/counter-app/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+
+class App extends Component {
+  state = {
+    //does not remove a counter from the array,
+    //instead create a new array without a given counter.
+    //Call the set state method of the component and
+    //let React update the state
+
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  
+    //call the contructor of the parent class using super();
+    constructor() {
+      super();
+      console.log('App - Constructor');
+    }
+
+  componentDidMount() {
+    //Ajax Call
+    console.log('App - Mounted');
+  }
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id != counterId);
+    this.setState({ counters });
+  };
+
+  //onReset is an event that are raised by the Counters component and
+  //this.handleReset is the corresponding handlers that belong to the app component
+  render() {
+    console.log('App - Rendered');
+
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
